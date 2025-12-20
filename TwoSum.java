3d30636c702159aa5a7d5d8bd160f61870c418a1
Prathameshghorapade <pghorapade17@gmail.com>
2025-12-20 22:23:32 +0530

    day 1

---
+++ TwoSum.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int arry[]={1,2,3,4,5,6,9};
+        int target=9;
+
+        System.out.println("Index :"+Arrays.toString(twoSum(arry,target)));
+
+    }
+
+
+    public static int[] twoSum(int[] arry,int target){
+
+        Map<Integer,Integer>map=new HashMap<>();
+
+        for(int i=0;i<arry.length;i++){
+            int complement=target-arry[i];
+
+
+            if (map.containsKey(complement)){
+             return new int[]{map.get(complement),i};
+            }
+
+            map.put(arry[i],i);
+
+        }
+        return new int[]{};
+
+    }
+}
