291c8a28f82536849d153402c65b3da6dfb49945
Prathameshghorapade <pghorapade17@gmail.com>
2026-01-04 01:30:43 +0530

    Day 7.

--- Factorial.java
+++ Factorial.java
@@ -4,28 +4,33 @@ public class Factorial {
     public static void main(String[] args) {
 
 
-//        int number=5;
-//        int result=1;
-//        for (int i=1;i<=number;i++){
-//
-//            result=result*i;
-//        }
-//        System.out.println(result);
-//    }
+        int number =5;
+
+        int result=1;
+
+        for (int i = 1; i <=number ; i++) {
+
+            result=result*i;
+        }
+
+        System.out.println("Factorial of " +number +" is "+result);
+
+
+
 
         // if the number is bigger use BIGINTEGER
 
-        int number=21;
-
-        BigInteger result=BigInteger.ONE;
-
-        for (int i = 1; i <=number; i++) {
-
-            result=result.multiply(BigInteger.valueOf(i));
-        }
-
-        System.out.println(result);
-
-
-    }
+//        int number=21;
+//
+//        BigInteger result=BigInteger.ONE;
+//
+//        for (int i = 1; i <=number; i++) {
+//
+//            result=result.multiply(BigInteger.valueOf(i));
+//        }
+//
+//        System.out.println(result);
+//
+//
+  }
 }
