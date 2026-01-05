52c4204e7374cd5d2651f709f4d1ec01e1189784
Prathameshghorapade <pghorapade17@gmail.com>
2026-01-05 21:52:15 +0530

    Day 9

---
+++ Pangram.java
@@ -0,0 +1,28 @@
+public class Pangram {
+    public static void main(String[] args) {
+
+        String str="abcdefghijklmnopqrstuvwxyz";
+
+        str=str.toLowerCase();
+
+        System.out.println(isPangram(str));
+
+    }
+
+    static boolean isPangram(String input){
+
+        if (input.length()<26){
+            return false;
+        }
+
+        for (int i = 'a'; i <='z' ; i++) {
+           if (input.indexOf(i) <0){
+               return false;
+
+           }        }
+
+
+        return true;
+
+    }
+}
