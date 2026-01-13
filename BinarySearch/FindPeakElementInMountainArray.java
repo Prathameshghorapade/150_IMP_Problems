0e76cd7f2bc1054536c18238394dd5b7bb942c0f
Prathameshghorapade <pghorapade17@gmail.com>
2026-01-13 12:58:43 +0530

    Day 16

--- BinarySearch/FindPeakElementInMountainArray.java
+++ BinarySearch/FindPeakElementInMountainArray.java
@@ -3,7 +3,7 @@ package BinarySearch;
 public class FindPeakElementInMountainArray {
     public static void main(String[] args) {
 
-        int[]array={1,2,3,4,5,4,3,2,1};
+        int[]array={1,2,3,4,5,6,4,3,2,1};
 
         System.out.println(findPeak(array));
 
@@ -14,19 +14,17 @@ public class FindPeakElementInMountainArray {
         int start=0;
         int end=array.length-1;
 
-        while (start<=end){
+        while (start<end){
             int mid=start+(end-start)/2;
 
-            if (mid!=0 && mid!=end && array[mid]>array[mid-1] && array[mid]>array[mid+1]){
-                return array[mid];
-            } else if (mid!=end && array[mid]<array[mid+1]) {
-                start=mid+1;
-            }else {
-                end=mid-1;
-            }
+           if (array[mid]<array[mid+1]){
+               start=mid+1;
+           }else {
+               end=mid;
+           }
         }
 
-        return -1;
+        return array[start];
 
 
 
