b805f5450b1b8edb1a180c21cfff68cd3da48105
Prathameshghorapade <pghorapade17@gmail.com>
2026-01-13 11:52:08 +0530

    Day 16

---
+++ BinarySearch/ValidMountainArray.java
@@ -0,0 +1,53 @@
+package BinarySearch;
+
+//Mountain array with Strictly Increasing and Strictly Decresing
+public class ValidMountainArray {
+    public static void main(String[] args) {
+
+        int[]array1={1,2,3,4,5,4,3,2,1};
+        System.out.println(isMountainArray(array1));
+
+        int[]array2={1,2};
+        System.out.println(isMountainArray(array2));
+
+        int []array3={2,3,4,5,5,4,3};
+        System.out.println(isMountainArray(array2));
+
+    }
+
+    static boolean isMountainArray(int[]array){
+
+        int index=0;
+
+        int end=array.length-1;
+
+        if (array.length<3){
+            return false;
+        }
+
+        while (index<end) {
+
+            if (array[index] < array[index + 1]) {
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        if (index==0 || index==end){
+            return false;
+        }
+
+        while (index<end){
+            if (array[index]>array[index+1]){
+                index++;
+            }else {
+                break;
+            }
+        }
+
+        return (index==end);
+
+
+    }
+}
